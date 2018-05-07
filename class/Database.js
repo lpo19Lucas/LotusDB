62e8a8abd627a9c26bc669088f112fbad83052ca
lpo19Lucas <lucas.oliveira@catho.com>
2018-05-07 12:39:28 -0300

    refacting openDb to cache the json file

--- class/Database.js
+++ class/Database.js
@@ -1,32 +1,58 @@
 const fs = require('fs'); 
 module.exports = class Database {
-  openDb(){
-    fs.readFile('./db/var/db.json', 'utf8',  (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(data);
-    });  
+
+  constructor(url) {
+    this.url = url;
+  }
+
+  cached(data) {
+    this.cache = data;
+    console.log(cache)
+  }
+
+  openDb() {
+    const fileContent = fs.readFileSync(this.url).toString('utf8');
+    if (!fileContent) {
+      this.cache = {};
+    } else {
+      this.cache = JSON.parse(fileContent);
+    }  
   }
  
   readString() {
     const content = fs.readFileSync('./db/var/db.json');
-    return content.toString();
+    return content.toString('utf-8');
   }
   
   readJson() {
-    const content = fs.readFileSync('./db/var/db.json');
-    const json = JSON.parse(content.toString('utf-8'));
+    const json = JSON.parse(this.readString());
+    this.cache(json)
     return json;
   }
 
-  insert(str){
+  insert(object){
+    let json = this.readJson();
+    let newObject = Object.keys(json).length;
+    json[newObject] = object;
+    let str = JSON.stringify(json)
     fs.writeFile('./db/var/db.json', str, (err) => {
       if (err){
-        console.log('Algo deu errado, o objeto não foi adicionado')
+        return true;
       }   
-      console.log('O objeto foi adicionado com sucesso!');
+      return false;
     });
   }
+
+  
+
+/*  cache.prototype.getCache = function () {
+    return cache;
+  };
+
+  getCache(){
+    return this.cache
+  }
+*/
 } 
+
 
